2782ede8cf0b3bed79de4e31878a54f35ad4217f
Paulo Henrique <phenriq694@gmail.com>
2020-05-02 01:48:18 -0300

    Section 3: Selecting multiple elements

--- scrapingIntro/index.js
+++ scrapingIntro/index.js
@@ -2,7 +2,7 @@ const request = require("request-promise");
 const cheerio = require("cheerio");
 const fs = require("fs");
 
-async function main() {
+async function selectAndPrintOneElement() {
   const html = await request.get(
     "https://reactnativetutorial.net/css-selectors"
   ); 
@@ -12,6 +12,30 @@ async function main() {
   const $ = await cheerio.load(html);
   const theText = $("h1").text();
   console.log(theText);
+}
+
+async function selectAndPrintMultipleElements() {
+  const html = await request.get(
+    "https://reactnativetutorial.net/css-selectors/lesson2.html"
+  ); 
+
+  fs.writeFileSync("./test2.html", html);
+  
+  const $ = await cheerio.load(html);
+  $("h2").each((index, element) => console.log($(element).text()));
+}
+
+async function main() {
+  // Output: Simple select of HTML element
+  await selectAndPrintOneElement();
+
+  /** 
+   * Output:
+   * I'm a h2 element
+   * I'm also a h2 element
+   * I'm a h2 element too!
+  */
+  selectAndPrintMultipleElements();
 }
 
 main()
