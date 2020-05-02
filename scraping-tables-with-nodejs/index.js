1ca74e81952490acbcba65445bda5aaab0961708
Paulo Henrique <phenriq694@gmail.com>
2020-05-02 02:37:44 -0300

    Section 4: Scraping data in Nodejs with Cheerio/Request

---
+++ scraping-tables-with-nodejs/index.js
@@ -0,0 +1,16 @@
+const request = require("request-promise");
+const cheerio = require("cheerio");
+
+async function main() {
+  const result = await request.get(
+    "https://www.codingwithstefan.com/table-example"
+  );
+
+  const $ = cheerio.load(result);
+
+  $("body > table > tbody > tr > td").each((index, element) => {
+    console.log($(element).text());
+  });
+}
+
+main();
