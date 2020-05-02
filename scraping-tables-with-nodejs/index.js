aa7bba9690f59c82dae307a179a7bdc79f5d865d
Paulo Henrique <phenriq694@gmail.com>
2020-05-02 02:51:57 -0300

    Section 4: Scraping Company Names in Nodejs

--- scraping-tables-with-nodejs/index.js
+++ scraping-tables-with-nodejs/index.js
@@ -8,8 +8,18 @@ async function main() {
 
   const $ = cheerio.load(result);
 
-  $("body > table > tbody > tr > td").each((index, element) => {
-    console.log($(element).text());
+  // $("body > table > tbody > tr > td").each((index, element) => {
+  //   console.log($(element).text());
+  // });
+
+  // Selecting all table rows
+  $("body > table > tbody > tr").each((index, element) => {
+    // Selecting all row cells
+    const tds = $(element).find("td"); 
+
+    // Selecting the first cell data from the row
+    const company = $(tds[0]).text();
+    console.log(company);
   });
 }
 
