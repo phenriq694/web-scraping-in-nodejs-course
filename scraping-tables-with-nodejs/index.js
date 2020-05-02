821bddf7f04aaa866ee19158ba2792e48dc58626
Paulo Henrique <phenriq694@gmail.com>
2020-05-02 03:04:46 -0300

    Section 4: Scraping all table columns

--- scraping-tables-with-nodejs/index.js
+++ scraping-tables-with-nodejs/index.js
@@ -7,20 +7,30 @@ async function main() {
   );
 
   const $ = cheerio.load(result);
-
+  const scrapedRows = [];
   // $("body > table > tbody > tr > td").each((index, element) => {
   //   console.log($(element).text());
   // });
 
   // Selecting all table rows
   $("body > table > tbody > tr").each((index, element) => {
+    // Avoind the first row 
+    if (index === 0) return true; 
+    
     // Selecting all row cells
     const tds = $(element).find("td"); 
 
     // Selecting the first cell data from the row
     const company = $(tds[0]).text();
-    console.log(company);
+
+    const contact = $(tds[1]).text();
+    const country = $(tds[2]).text();
+
+    const scrapedRow = { company, contact, country };
+    scrapedRows.push(scrapedRow);
   });
+
+  console.log(scrapedRows);
 }
 
 main();
