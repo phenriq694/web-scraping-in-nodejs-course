24d6172bf52d9fb8b7b4f42e05d062c13b9369b0
Paulo Henrique <phenriq694@gmail.com>
2020-05-05 01:01:54 -0300

    Section 6: Title/URL form jobs.

--- webScraping-using-request/index.js
+++ webScraping-using-request/index.js
@@ -2,14 +2,27 @@ const request = require('request-promise');
 const cheerio = require('cheerio');
 
 const url = 'https://indeed.com/q-Web-Developer-l-United-States-jobs.html';
+const scrapeResult = {};
+const scrapeResults = [];
 
-async function scrapeCraigslist() {
+async function main() {
   try {
     const htmlResult = await request.get(url);
     const $ = cheerio.load(htmlResult);
+
+    $(".jobsearch-SerpJobCard").each((index, element) => {
+      const resultTitle = $(element).children(".title");
+      const title = resultTitle.text().trim();
+      const jobUrl = resultTitle.children(".jobtitle").attr("href");
+
+      const scrapeResult = { title, url };
+      scrapeResults.push(scrapeResult);
+    });
+
+    console.log(scrapeResults);
   } catch (err) {
     console.error(err);
   }
 }
 
-scrapeCraigslist()
+main()
