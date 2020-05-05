ffe4131db0d86abfb6666c1e6b17d421dc69763c
Paulo Henrique <phenriq694@gmail.com>
2020-05-05 02:18:36 -0300

    Section 6: Scraping Job Descriptions.

--- webScraping-using-request/index.js
+++ webScraping-using-request/index.js
@@ -2,10 +2,10 @@ const request = require('request-promise');
 const cheerio = require('cheerio');
 
 const url = 'https://indeed.com/q-Web-Developer-l-United-States-jobs.html';
-const scrapeResult = {};
-const scrapeResults = [];
+const jobSummaray = {};
+const jobSummaries = [];
 
-async function main() {
+async function scrapeJobSummay () {
   try {
     const htmlResult = await request.get(url);
     const $ = cheerio.load(htmlResult);
@@ -13,17 +13,34 @@ async function main() {
     $(".jobsearch-SerpJobCard").each((index, element) => {
       const resultTitle = $(element).children(".title");
       const title = resultTitle.text().trim();
-      const jobUrl = resultTitle.children(".jobtitle").attr("href");
+      const jobUrl = "https://www.indeed.com" + resultTitle.children(".jobtitle").attr("href");
       const datePosted = $(element).find(".date ").text();
       const location = $(element).find(".location").text();
-      const scrapeResult = { title, jobUrl, datePosted, location };
-      scrapeResults.push(scrapeResult);
+
+      const jobSummaray = { title, jobUrl, datePosted, location };
+      jobSummaries.push(jobSummaray);
     });
 
-    console.log(scrapeResults);
+    return jobSummaries
   } catch (err) {
     console.error(err);
   }
+}
+
+async function scrapeJobDescription(jobSummaries) {
+  jobSummaries.map(async job => {
+    const htmlResult = await request.get(job.url);
+    const $ = await cheerio.load(htmlResult);
+
+    $("#jobDescriptionText").each((index, element) => {
+      job.description = $(element).find("p").text();
+    });
+  }
+}
+
+async function main() {
+  const jobSumaries = await scrapeJobSummay();
+  console.log(jobSumaries)
 }
 
 main()
