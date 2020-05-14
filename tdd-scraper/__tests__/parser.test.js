c4084c2c043245e749b276ca4a0d57cf5cd8f082
Paulo Henrique <phenriq694@gmail.com>
2020-05-13 23:52:54 -0300

    Section 8: Writing out our tests

--- tdd-scraper/__tests__/parser.test.js
+++ tdd-scraper/__tests__/parser.test.js
@@ -1,12 +1,21 @@
 const parser = require("../parser");
 const fs = require("fs");
 let html;
+let categories;
 
 beforeAll(() => {
-  let html = fs.readFileSync("./test.html");
+  html = fs.readFileSync("./test.html");
+  categories = parser.categories(html);
 });
 
-it("should give 4", () => {
-  const result = parser.add(2, 2);
-  expect(result).toBe(4);
+it("should give the correct number of categories", () => {
+  expect(categories.length.toBe(9));
+});
+
+it("should get correct title", () => {
+  expect(categories[0].title).toBe("Arts");
+});
+
+it("should get correct url", () => {
+  expect(categories[0].url).toBe("https://en.wikipedia.org/wiki/Portal:Arts");
 });
